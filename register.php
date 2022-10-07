0323bf0e89b3dd261869047e4ba16733b15505f6
h07850225 <h07850225@gmail.com>
2022-10-07 21:25:49 +0800

    登入功能PHP上傳

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
-    header(`Access-Control-Allow-Origin: *`);//跨網域，需要這段才不會被攔截
-    //header("Content-Type:application/json;charset=utf-8");
+    header("Access-Control-Allow-Origin:*");//跨網域，需要這段才不會被攔截
+    // header("Content-Type:application/x-www-form-urlencoded");
     //$content = trim(file_get_contents("php://input"));
     //$data = json_decode($content);
 
@@ -22,39 +22,36 @@
                   VALUES('{$mem_id}','{$mem_psw}','{$mem_name}','{$mem_nick_name}','{$mem_email}','{$mem_city}','{$mem_addr}','{$mem_phone}',1,1,now())";
             $pdo->exec($sql);
 
-            //write into session
-            $_SESSION["mem_no"]=$mem_no;
-            $_SESSION["mem_id"]=$mem_id;
-            $_SESSION["mem_name"]=$mem_name;
-            $_SESSION["mem_nick_name"]=$mem_nick_name;
-            $_SESSION["mem_city"]=$mem_city;
-            $_SESSION["mem_addr"]=$mem_addr;
-            $_SESSION["mem_phone"]=$mem_phone;
-            $_SESSION["mem_pic"]=$mem_pic;
-            $_SESSION["mem_status"]=$mem_status;
-            $_SESSION["register_date"]=$register_date;
-            //pack data
-            $result = ["mem_no"=>$_SESSION["mem_no"],
-                       "mem_id"=>$_SESSION["mem_id"],
-                       "mem_name"=>$_SESSION["mem_name"],
-                       "mem_email"=>$_SESSION["mem_email"],
-                       "mem_nick_name"=>$_SESSION["mem_nick_name"],
-                       "mem_city"=>$_SESSION["mem_city"],
-                       "mem_addr"=>$_SESSION["mem_addr"],
-                       "mem_phone"=>$_SESSION["mem_phone"],
-                       "mem_status"=>$_SESSION["mem_status"],
-                       "register_date"=>$_SESSION["register_date"],
-                       "mem_pic"=>$_SESSION["mem_pic"]];
-            echo json_encode($result);
+            // //write into session
+            // $_SESSION["mem_no"]=$mem_no;
+            // $_SESSION["mem_id"]=$mem_id;
+            // $_SESSION["mem_name"]=$mem_name;
+            // $_SESSION["mem_nick_name"]=$mem_nick_name;
+            // $_SESSION["mem_city"]=$mem_city;
+            // $_SESSION["mem_addr"]=$mem_addr;
+            // $_SESSION["mem_phone"]=$mem_phone;
+            // //pack data
+            // $result = ["mem_no"=>$_SESSION["mem_no"],
+            //            "mem_id"=>$_SESSION["mem_id"],
+            //            "mem_name"=>$_SESSION["mem_name"],
+            //            "mem_email"=>$_SESSION["mem_email"],
+            //            "mem_nick_name"=>$_SESSION["mem_nick_name"],
+            //            "mem_city"=>$_SESSION["mem_city"],
+            //            "mem_addr"=>$_SESSION["mem_addr"],
+            //            "mem_phone"=>$_SESSION["mem_phone"]];
+            // echo json_encode($result);
+            echo "1";
             }
-             }else{ //找得到
+            else{ //找得到
             //傳回此帳號已存在
-              echo "此帳號已存在";
-            }	
+              echo "0";
+            }}	
             catch(PDOException $e){
                 echo $e->getMessage();
             }
         }
-        createMember($_POST['mem_id'],$_POST['mem_psw'],$_POST['mem_name'],$_POST['mem_nick_name'],$_POST['mem_email'],$_POST['mem_city'],$_POST['mem_addr'],$_POST['mem_phone'])
+        createMember(
+            $_POST['mem_id'],$_POST['mem_psw'],$_POST['mem_name'],$_POST['mem_nick_name'],$_POST['mem_email'],$_POST['mem_city'],$_POST['mem_addr'],$_POST['mem_phone']
+            )
       
 ?>
